2d01f52f2bb23bec586bd2cf71d38b3759e75b4e
tilz0R <tilen.majerle@gmail.com>
2018-01-26 17:59:04 +0100

    Move all structures and enumerations to typedef file

--- src/include/esp/esp.h
+++ src/include/esp/esp.h
@@ -37,7 +37,13 @@
 extern "C" {
 #endif /* __cplusplus */
 
-/* Include other library files */
+/**
+ * \defgroup        ESP ESP AT lib
+ * \brief           ESP stack
+ * \{
+ */
+ 
+/* Setup config file */
 #include "esp_config.h"
 
 #include "esp/esp_typedefs.h"
@@ -45,185 +51,15 @@ extern "C" {
 #include "esp/esp_input.h"
 #include "system/esp_sys.h"
 #include "esp/esp_debug.h"
-
-/**
- * \defgroup        ESP ESP AT lib
- * \brief           ESP stack
- * \{
- */
- 
-/**
- * \addtogroup      ESP_LL
- * \{
- */
- 
-/**
- * \brief           Function prototype for AT output data
- * \param[in]       data: Pointer to data to send
- * \param[in]       len: Number of bytes to send
- * \return          Number of bytes sent
- */
-typedef uint16_t (*esp_ll_send_fn)(const void* data, uint16_t len);
-
-/**
- * \brief           Low level user specific functions
- */
-typedef struct {
-    esp_ll_send_fn send_fn;                     /*!< Callback function to transmit data */
-} esp_ll_t;
-
-/**
- * \}
- */
-
-/**
- * \addtogroup      ESP_TYPEDEFS
- * \{
- */
-
-/**
- * \brief           List of possible WiFi modes
- */
-typedef enum {
-#if ESP_CFG_MODE_STATION || __DOXYGEN__
-    ESP_MODE_STA = 1,                           /*!< Set WiFi mode to station only */
-#endif /* ESP_CFG_MODE_STATION || __DOXYGEN__ */
-#if ESP_CFG_MODE_ACCESS_POINT || __DOXYGEN__
-    ESP_MODE_AP = 2,                            /*!< Set WiFi mode to access point only */
-#endif /* ESP_CFG_MODE_ACCESS_POINT || __DOXYGEN__ */
-#if (ESP_CFG_MODE_STATION_ACCESS_POINT) || __DOXYGEN__
-    ESP_MODE_STA_AP = 3,                        /*!< Set WiFi mode to station and access point */
-#endif /* (ESP_CFG_MODE_STATION_ACCESS_POINT) || __DOXYGEN__ */
-} esp_mode_t;
-
-/**
- * \brief           List of possible connection types
- */
-typedef enum {
-    ESP_CONN_TYPE_TCP,                          /*!< Connection type is TCP */
-    ESP_CONN_TYPE_UDP,                          /*!< Connection type is UDP */
-    ESP_CONN_TYPE_SSL,                          /*!< Connection type is SSL */
-} esp_conn_type_t;
-
-/* Forward declarations */
-struct esp_cb_t;
-struct esp_conn_t;
-struct esp_pbuf_t;
-
-/**
- * \brief           Pointer to \ref esp_conn_t structure
- */
-typedef struct esp_conn_t* esp_conn_p;
-
-/**
- * \brief           Pointer to \ref esp_pbuf_t structure
- */
-typedef struct esp_pbuf_t* esp_pbuf_p;
-
-/**
- * \brief           Event function prototype
- * \param[in]       cb: Callback event data
- * \return          espOK on success, member of \ref espr_t otherwise
- */
-typedef espr_t  (*esp_cb_fn)(struct esp_cb_t* cb);
-
-/**
- * \brief           List of possible callback types received to user
- */
-typedef enum esp_cb_type_t {
-    ESP_CB_RESET,                               /*!< Device reset detected */
-    
-    ESP_CB_INIT_FINISH,                         /*!< Initialization has been finished at this point */
-    
-    ESP_CB_CONN_DATA_RECV,                      /*!< Connection data received */
-    ESP_CB_CONN_DATA_SENT,                      /*!< Data were successfully sent */
-    ESP_CB_CONN_DATA_SEND_ERR,                  /*!< Error trying to send data */
-    ESP_CB_CONN_ACTIVE,                         /*!< Connection just became active */
-    ESP_CB_CONN_ERROR,                          /*!< Client connection start was not successful */
-    ESP_CB_CONN_CLOSED,                         /*!< Connection was just closed */
-    ESP_CB_CONN_POLL,                           /*!< Poll for connection if there are any changes */
-    
-#if ESP_CFG_MODE_STATION || __DOXYGEN__
-    ESP_CB_WIFI_CONNECTED,                      /*!< Station just connected to AP */
-    ESP_CB_WIFI_GOT_IP,                         /*!< Station has valid IP */
-    ESP_CB_WIFI_DISCONNECTED,                   /*!< Station just disconnected from AP */
-  
-    ESP_CB_STA_LIST_AP,                         /*!< Station listed APs event */
-#endif /* ESP_CFG_MODE_STATION || __DOXYGEN__ */
-#if ESP_CFG_MODE_ACCESS_POINT || __DOXYGEN__
-    ESP_CB_AP_CONNECTED_STA,                    /*!< New station just connected to ESP's access point */
-    ESP_CB_AP_DISCONNECTED_STA,                 /*!< New station just disconnected from ESP's access point */
-    ESP_CB_AP_IP_STA,                           /*!< New station just received IP from ESP's access point */
-#endif /* ESP_CFG_MODE_ACCESS_POINT || __DOXYGEN__ */
-} esp_cb_type_t;
-
-/**
- * \brief           Global callback structure to pass as parameter to callback function
- */
-typedef struct esp_cb_t {
-    esp_cb_type_t type;                         /*!< Callback type */
-    union {
-        struct {
-            esp_conn_p conn;                    /*!< Connection where data were received */
-            esp_pbuf_p buff;                    /*!< Pointer to received data */
-        } conn_data_recv;                       /*!< Network data received. Use with \ref ESP_CB_CONN_DATA_RECV event */
-        struct {
-            esp_conn_p conn;                    /*!< Connection where data were sent */
-            size_t sent;                        /*!< Number of bytes sent on connection */
-        } conn_data_sent;                       /*!< Data successfully sent. Use with \ref ESP_CB_CONN_DATA_SENT event */
-        struct {
-            esp_conn_p conn;                    /*!< Connection where data were sent */
-            size_t sent;                        /*!< Number of bytes sent on connection before error occurred */
-        } conn_data_send_err;                   /*!< Data were not sent. Use with \ref ESP_CB_CONN_DATA_SEND_ERR event */
-        struct {
-            const char* host;                   /*!< Host to use for connection */
-            esp_port_t port;                    /*!< Remote port used for connection */
-            esp_conn_type_t type;               /*!< Connection type */
-            void* arg;                          /*!< Connection argument used on connection */
-            /**
-             * \todo: Implement error reason:
-             *  - No free connection to start
-             *  - Remote host is not responding
-             *  - ...
-             */
-        } conn_error;                           /*!< Client connection start error */
-        struct {
-            esp_conn_p conn;                    /*!< Pointer to connection */
-            uint8_t client;                     /*!< Set to 1 if connection is/was client mode */
-            uint8_t forced;                     /*!< Set to 1 if connection action was forced (when active, 1 = CLIENT, 0 = SERVER; when closed, 1 = CMD, 0 = REMOTE) */
-        } conn_active_closed;                   /*!< Process active and closed statuses at the same time. Use with \ref ESP_CB_CONN_ACTIVE or \ref ESP_CB_CONN_CLOSED events */
-        struct {
-            esp_conn_p conn;                    /*!< Set connection pointer */
-        } conn_poll;                            /*!< Polling active connection to check for timeouts. Use with \ref ESP_CB_CONN_POLL event */
-        struct {
-            espr_t status;                      /*!< Status of command */
-            esp_ap_t* aps;                      /*!< Pointer to access points */
-            size_t len;                         /*!< Number of access points found */
-        } sta_list_ap;
-        
-#if ESP_CFG_MODE_ACCESS_POINT
-        struct {
-            esp_mac_t* mac;                     /*!< Station MAC address */
-        } ap_conn_disconn_sta;                  /*!< A new station connected or disconnected to ESP's access point. Use with \ref ESP_CB_AP_CONNECTED_STA or \ref ESP_CB_AP_DISCONNECTED_STA events */
-        struct {
-            esp_mac_t* mac;                     /*!< Station MAC address */
-            esp_ip_t* ip;                       /*!< Station IP address */
-        } ap_ip_sta;                            /*!< Station got IP address from ESP's access point. Use with \ref ESP_CB_AP_IP_STA */
-#endif /* ESP_CFG_MODE_ACCESS_POINT */
-    } cb;                                       /*!< Callback event union */
-} esp_cb_t;
-
-#define ESP_SIZET_MAX                           ((size_t)(-1))  /*!< Maximal value of size_t variable type */
-
-/**
- * \}
- */
-
 #include "esp/esp_utilities.h"
 #include "esp/esp_pbuf.h"
 #include "esp/esp_conn.h"
+#if ESP_CFG_MODE_STATION
 #include "esp/esp_sta.h"
+#endif /* ESP_CFG_MODE_STATION */
+#if ESP_CFG_MODE_ACCESS_POINT
 #include "esp/esp_ap.h"
+#endif /* ESP_CFG_MODE_ACCESS_POINT */
 #if ESP_CFG_OS
 #include "esp/esp_netconn.h"
 #endif /* ESP_CFG_OS */
@@ -239,7 +75,6 @@ typedef struct esp_cb_t {
 #if ESP_CFG_HOSTNAME
 #include "esp/esp_hostname.h"
 #endif /* ESP_CFG_HOSTNAME */
-
 
 espr_t      esp_init(esp_cb_fn cb_func);
 espr_t      esp_reset(uint32_t blocking);
