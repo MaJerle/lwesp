bd6e10821dcf433258ba16cbe5a1fc63d0f8cea9
tilz0R <tilen.majerle@gmail.com>
2018-02-05 18:12:14 +0100

    Update requirements docs page

--- docs/src/_requirements.h
+++ docs/src/_requirements.h
@@ -7,7 +7,7 @@
  * Development of this library follows latest ESP8266 AT software development by Espressif systems.
  *
  *  - ESP8266 must include latest AT software
- *      - Currently supported AT version is <b>1.6.0</b> based on SDK version `2.2.0` which is not yet officially released by EspressIf Systems
+ *      - Currently supported AT version is `1.6.0` based on SDK version `2.2.0` which is not yet officially released by EspressIf Systems
  *      - Precompiled development AT version from Espressif is available on Github of this library: https://github.com/MaJerle/ESP_AT_Lib/tree/master/bin
  *      - If you are not sure which version is running on ESP, you may test it using `AT+GMR\r\n` command.
  *
