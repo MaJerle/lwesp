27ac1398747c1cddcdd9983be976d4784a182548
tilz0R <tilen.majerle@gmail.com>
2018-07-02 20:13:14 +0200

    Remove event example comments

--- docs/examples/_example_evt.c
+++ docs/examples/_example_evt.c
@@ -6,7 +6,6 @@ espr_t
 esp_callback_function(esp_evt_t* evt) {
     switch (esp_evt_get_type(evt)) {
         case ESP_EVT_RESET: {                    /* Reset detected on ESP device */
-            /* Option 1 */
             if (esp_evt_reset_is_forced(evt)) {  /* Check if forced by user */
                 printf("Reset forced by user!\r\n");
             }
