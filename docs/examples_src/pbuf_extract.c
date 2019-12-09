83aada50abc9748826055f76becd1656a78a6dff
Tilen Majerle <tilen.majerle@gmail.com>
2019-12-09 23:21:06 +0100

    Fix code style

--- docs/examples_src/pbuf_extract.c
+++ docs/examples_src/pbuf_extract.c
@@ -22,28 +22,26 @@ esp_pbuf_chain(a, c);
 
 /*
  * pbuf a now contains chain of b and c together
- * and at this point we want to print (or save) data from chained pbuf
+ * and at this point application wants to print (or save) data from chained pbuf
  *
- * Process pbuf by pbuf
+ * Process pbuf by pbuf with code below
+ */
+
+/*
+ * Get linear address of current pbuf at specific offset
+ * Function will return pointer to memory address at specific position
+ * and `len` will hold length of data block
  */
 pos = 0;
-do {
-    /*
-     * Get linear address of current pbuf at specific offset
-     * Function will return pointer to memory address at specific position
-     * and will save length of data block
-     */
-    data = esp_pbuf_get_linear_addr(a, pos, &len);
-    if (data != NULL) {
-        /* Custom process function... */
-        /* Process data with data pointer and block length */
-        process_data(data, len);
-        printf("Str: %.*s", len, data);
+while ((data = esp_pbuf_get_linear_addr(a, pos, &len)) != NULL) {
+    /* Custom process function... */
+    /* Process data with data pointer and block length */
+    process_data(data, len);
+    printf("Str: %.*s", len, data);
 
-        /* Increase offset position for next block */
-        pos += len;
-    }
-} while (data != NULL);
+    /* Increase offset position for next block */
+    pos += len;
+}
 
-/* Call free only on a pbuf. Since we have chain, b and c will be freed aswell */
+/* Call free only on a pbuf. Since it is chained, b and c will be freed too */
 esp_pbuf_free(a);
