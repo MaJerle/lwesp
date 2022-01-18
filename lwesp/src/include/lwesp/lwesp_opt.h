3e98f91d5bde877aa068e9c9ec8d9384a49b33db
Tilen Majerle <tilen.majerle@gmail.com>
2022-01-18 21:41:56 +0100

    Fix minimum Espressif AT versions

--- lwesp/src/include/lwesp/lwesp_opt.h
+++ lwesp/src/include/lwesp/lwesp_opt.h
@@ -868,7 +868,7 @@ void *  my_memset(void* dst, int b, size_t len);
 #define LWESP_MIN_AT_VERSION_MINOR_ESP8266      2   /*!< Minimal minor version for ESP8266 */
 #define LWESP_MIN_AT_VERSION_PATCH_ESP8266      1   /*!< Minimal patch version for ESP8266 */
 #define LWESP_MIN_AT_VERSION_MAJOR_ESP32        2   /*!< Minimal major version for ESP32 */
-#define LWESP_MIN_AT_VERSION_MINOR_ESP32        3   /*!< Minimal minor version for ESP32 */
+#define LWESP_MIN_AT_VERSION_MINOR_ESP32        2   /*!< Minimal minor version for ESP32 */
 #define LWESP_MIN_AT_VERSION_PATCH_ESP32        0   /*!< Minimal patch version for ESP32 */
 #define LWESP_MIN_AT_VERSION_MAJOR_ESP32_C3     2   /*!< Minimal major version for ESP32-C3 */
 #define LWESP_MIN_AT_VERSION_MINOR_ESP32_C3     3   /*!< Minimal minor version for ESP32-C3 */
